41f0b316ca1775ee679a9723f63d31161be4fa4d
Miguel <miguelmartinezmiquel@gmail.com>
2024-05-29 17:42:06 +0200

    Automatic plot done.

--- Info_Grover_general.py
+++ Info_Grover_general.py
@@ -42,7 +42,7 @@ def grover_oracle(marked_states):
 #%% Evolving quantum states through the circuit
 
 # Circuit details
-marked_states = ['1010000000']
+marked_states = ['1010000']
 num_qubits = len(marked_states[0])
 oracle = grover_oracle(marked_states)
 grover_op = GroverOperator(oracle, insert_barriers=True)
@@ -53,51 +53,37 @@ n_iter = 8
 # Initial state
 qc = QuantumCircuit(num_qubits)
 qc.h(range(num_qubits))
-psi0 = Statevector.from_label('0000000000')
+psi0 = Statevector.from_label('0000000')
+info_dict[0] = calc_info(psi0.evolve(qc).data)
 
 # Grover iterations
-for i in range(0, n_iter):
+for i in range(1, n_iter + 1):
     qc.compose(grover_op, inplace=True)
-    info_dict[f'{i}'] = calc_info(psi0.evolve(qc).data)
+    info_dict[i] = calc_info(psi0.evolve(qc).data)
 
 
 #%% Figures
 fig1 = plt.figure(figsize=(8, 5))
-gs = GridSpec(2, 4, figure=fig1)
-ax0 = fig1.add_subplot(gs[0, 0])
-ax1 = fig1.add_subplot(gs[0, 1])
-ax2 = fig1.add_subplot(gs[0, 2])
-ax3 = fig1.add_subplot(gs[0, 3])
-ax4 = fig1.add_subplot(gs[1, 0])
-ax5 = fig1.add_subplot(gs[1, 1])
-ax6 = fig1.add_subplot(gs[1, 2])
-ax7 = fig1.add_subplot(gs[1, 3])
+gs = GridSpec(2, 5, figure=fig1)
+
+for i in range(n_iter + 1):
+    if ((n_iter + 1) % 2) == 0:
+        if i < int((n_iter + 1)/ 2):
+            ax = fig1.add_subplot(gs[0, i])
+        else:
+            ax = fig1.add_subplot(gs[1, i % int((n_iter + 1)/ 2)])
+    else:
+        if i <= int((n_iter + 1) / 2):
+            ax = fig1.add_subplot(gs[0, i])
+        elif i != (n_iter + 1) - 1:
+            ax = fig1.add_subplot(gs[1, (i % int((n_iter + 1) / 2)) - 1])
+        else:
+            ax = fig1.add_subplot(gs[1, int((n_iter + 1) / 2) - 1])
+
+    plot_info_latt(info_dict[i], ax)
+    ax.set_title(f'$G^{i}$')
+
 fig1.suptitle(f'Optimal iteration: {optimal_iter}')
-
-plot_info_latt(info_dict['0'], ax0)
-ax0.set_title(f'$G^1$')
-
-plot_info_latt(info_dict['1'], ax1)
-ax1.set_title(f'$G^2$')
-
-plot_info_latt(info_dict['2'], ax2)
-ax2.set_title(f'$G^3$')
-
-plot_info_latt(info_dict['3'], ax3)
-ax3.set_title(f'$G^4$')
-
-plot_info_latt(info_dict['4'], ax4)
-ax4.set_title(f'$G^5$')
-
-plot_info_latt(info_dict['5'], ax5)
-ax5.set_title(f'$G^6$')
-
-plot_info_latt(info_dict['6'], ax6)
-ax6.set_title(f'$G^7$')
-
-plot_info_latt(info_dict['7'], ax7)
-ax7.set_title(f'$G^8$')
-
 plt.show()
 
 
