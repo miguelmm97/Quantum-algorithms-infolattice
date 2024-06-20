dc844095402e41a11c1e3e8831be0bafd4f97da3
miguelmartinez <miguelmartinezmiquel@gmail.com>
2024-06-20 17:47:03 +0200

    Started deutsch

---
+++ deutsch-algorithm/deutsch-general.py
@@ -0,0 +1,25 @@
+#%% Imports
+
+# Math and plotting
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.gridspec import GridSpec
+import seaborn
+
+# Managing data
+import h5py
+import os
+import sys
+from datetime import date
+
+# Imports from Qiskit
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import GroverOperator, MCMT, ZGate, HGate
+from qiskit.quantum_info import Statevector
+from qiskit.quantum_info.operators import Operator
+
+# Information lattice
+from functions import (calc_info, plot_info_latt, calc_info_per_scale, get_fileID, store_my_data,
+                       attr_my_data, median)
+
